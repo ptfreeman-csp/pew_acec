31d905d73c595aeccca079418f3324c304dea216
CaitLittlef <caitlin.littlefield@uvm.edu>
2022-04-06 16:44:47 -0400

    misc

--- 02_randSel.R
+++ 02_randSel.R
@@ -1,3 +1,9 @@
+#######################################
+## GENERATE RANDOM SAMPLE OF PA AREA ##
+#######################################
+
+
+
 today <- paste0(mid(Sys.Date(),3,2),
                 mid(Sys.Date(),6,2),
                 mid(Sys.Date(),9,2))
@@ -6,7 +12,7 @@ domain <- biome # Domain from which to sample values.
 boundary <- ynp # Boundary of existing/proposed PA.
 
 # Compute target area of that PA. Convert from simple feature (sf) to spatial.
-trgt_area <- terra::area(as_Spatial(ynp)) # 8904687776 sqm; checks out.
+(trgt_area <- terra::area(as_Spatial(ynp))) # 8904687776 sqm; checks out.
 # boundary.r <- fasterize(boundary, amph) %>%
 #   crop(ynp) %>% mask(ynp) # Turn into raster but keep small.
 # area(boundary.r) # Not recommended as object is projected and not lat/long
@@ -24,47 +30,46 @@ pts = sf::st_sample(domain, size = n) ; str(pts)
 
 
 
-
-
-############# PRUNING OPTION #################
-# ref:# https://www.jla-data.net/eng/creating-and-pruning-random-points-and-polygons/
-# Prune the too-many points
-i <- 1 # iterator start
-
-buffer_size <- sqrt(trgt_area) # minimal distance to be enforced (in meters)
-
-repeat( {
-
-  #  create buffer around i-th point
-  buffer <- st_buffer(pts[i], buffer_size )
-
-  offending <- pts %>%  # start with the intersection of master points...
-    st_intersects(buffer, sparse = F) # ... and the buffer, as a vector
-
-  # i-th point is not really offending; retain it
-  offending[i] <- FALSE
-
-  # If there are any offending points left, re-assign the master points
-  # with the offending ones excluded. This is the main pruning part.
-  pts <- pts[!offending]
-
-  if ( i >= length(pts)) {
-    # the end was reached; no more points to process
-    break
-  } else {
-    # rinse & repeat
-    i <- i + 1
-  }
-
-} )
-
-pts <- pts[1:100] # Retain just the first 100.
-# These are now random-but-regular.
-
-#####################################################
+# ############# PRUNING OPTION #################
+# # ref:# https://www.jla-data.net/eng/creating-and-pruning-random-points-and-polygons/
+# # Prune the too-many points
+# i <- 1 # iterator start
+# 
+# buffer_size <- sqrt(trgt_area) # minimal distance to be enforced (in meters)
+# 
+# repeat( {
+# 
+#   #  create buffer around i-th point
+#   buffer <- st_buffer(pts[i], buffer_size )
+# 
+#   offending <- pts %>%  # start with the intersection of master points...
+#     st_intersects(buffer, sparse = F) # ... and the buffer, as a vector
+# 
+#   # i-th point is not really offending; retain it
+#   offending[i] <- FALSE
+# 
+#   # If there are any offending points left, re-assign the master points
+#   # with the offending ones excluded. This is the main pruning part.
+#   pts <- pts[!offending]
+# 
+#   if ( i >= length(pts)) {
+#     # the end was reached; no more points to process
+#     break
+#   } else {
+#     # rinse & repeat
+#     i <- i + 1
+#   }
+# 
+# } )
+# 
+# pts <- pts[1:100] # Retain just the first 100.
+# # These are now random-but-regular.
+# 
+# #####################################################
 
 
 ggplot() + 
+  # geom_raster(aes(), data = amph) +
   geom_sf(aes(), data=biome) + 
   geom_sf(aes(), data=pts)
 
@@ -73,10 +78,14 @@ ggplot() +
 # ?gBuffer
 sample <- gBuffer(as_Spatial(pts),
                width = sqrt(trgt_area)/2, # get length of area side, halve it
-               byid = TRUE, # keep them all separate polys
-               capStyle = "SQUARE") #%>% st_as_sf()
+               byid = TRUE) # keep them all separate polys
+               # capStyle = "SQUARE") %>% st_as_sf() ;
 
-plot(amph)
-plot(sample, add = TRUE) ; terra::area(sample[1])
-plot(ynp, add = TRUE) ; terra::area(as_Spatial(ynp))
+sample <- as(sample, "sf")
+
+par(mfrow=c(1,1))
+plot(mamm)
+plot(sample, add = TRUE) #; terra::area(sample[1])
+plot(ynp, add = TRUE) #; terra::area(as_Spatial(ynp))
 plot(pts, add = TRUE)
+
