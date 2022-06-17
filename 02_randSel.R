92dfde19743cc4ac5d3bd304d961181863ac506a
CaitLittlef <caitlin.littlefield@uvm.edu>
2022-06-17 10:40:20 -0400

    trivial; won't use in future

--- 02_randSel.R
+++ 02_randSel.R
@@ -2,26 +2,35 @@
 ## GENERATE RANDOM SAMPLE OF PA AREA ##
 #######################################
 
-today <- paste0(mid(Sys.Date(),3,2),
-                mid(Sys.Date(),6,2),
-                mid(Sys.Date(),9,2))
 
 domain <- west # Domain from which to sample values.
 boundary <- ls # Boundary of existing/proposed PA.
 
-# Compute target area of that PA. Convert from simple feature (sf) to spatial.
-(trgt_area <- terra::area(as_Spatial(boundary), unit = "m")) 
-# ^ THIS CAN'T BE LAT/LON
 
+# Compute target area of that PA. 
+(trgt_area <- terra::area(boundary, unit = "m")) 
 
-# The number of samples generated in each iteration
-n <- 1000 # consider starting with way too many then pruning.
-# n <- 100
+# Specify sample number; consider starting w lots then pruning (code at bottom)
+# n <- 1000 
+n <- 100
 # n <- 10
+
 
 # Generate n random points within domain
 pts = sf::st_sample(domain, size = n) ; str(pts)
-# pts = spsample(domain, n = n, type = random); str(pts)
+
+
+# Grow buffer around each point to be same size as PA; convert to sf
+sample <- gBuffer(as_Spatial(pts),
+               width = sqrt(trgt_area/3.14), # get length of area side, halve it
+               byid = TRUE) %>% st_as_sf() 
+
+
+plot(amph)
+plot(sample, add = TRUE)
+
+
+
 
 
 
@@ -61,21 +70,3 @@ pts = sf::st_sample(domain, size = n) ; str(pts)
 # # These are now random-but-regular.
 # 
 # #####################################################
-
-# 
-# ggplot() + 
-#   geom_sf(aes(), data=west) +
-#   geom_sf(aes(), data=pts)
-# 
-
-# Grow buffer around each point to be same size as PA
-# ?gBuffer
-sample <- gBuffer(as_Spatial(pts),
-               width = sqrt(trgt_area)/2, # get length of area side, halve it
-               byid = TRUE) # keep them all separate polys
-               # capStyle = "SQUARE") %>% st_as_sf() ;
-
-sample <- as(sample, "sf")
-
-
-plot(sample, add = TRUE) #; terra::area(sample[1])
