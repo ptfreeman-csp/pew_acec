eeebd1b34e78b3b25fe8ac51fab8ddcd5b4f0a48
CaitLittlef <caitlin.littlefield@uvm.edu>
2022-07-29 10:56:29 -0400

    tidy; acre conversion

--- 05_bonusPts.R
+++ 05_bonusPts.R
@@ -2,22 +2,32 @@
 ## CALC AREAS IN MIGRATION ROUTES & IBA ##
 ##########################################
 
+acre_in_km <- 247.105
+
 aoisShapes <- list(rd_allx, ls)
+aoisNames <- c(
+  "Red Desert",
+  "Little Sandy"
+)
 
-a <- aoiShapes[[1]]; print(aoisNames[1])
-a <- aoiShapes[[2]]; print(aoisNames[2])
-
-
-# Alt: could do extract val and take sum of pixels (0 or 1) for aoi and samples.
+a <- aoisShapes[[1]]; print(aoisNames[1])
+a <- aoisShapes[[2]]; print(aoisNames[2])
 
 # Area in square m in all Wyo; nb can't set km in area()
 migrArea <- terra::area(migr) %>% sum()#/1000000
 ibaArea <- terra::area(iba) %>% sum()#/1000000
 
-migrAreaAoi <- st_as_sf(migr) %>% st_intersection(st_as_sf(a)) %>%
-  as_Spatial() %>%terra::area() %>% sum()#/1000000
-ibaAreaAoi <- st_as_sf(iba) %>% st_intersection(st_as_sf(a)) %>%
-  as_Spatial() %>% terra::area() %>% sum()#/1000000
+# Get intersecting area; nb error may indicated ZERO overlap.
+migrAreaAoi <- st_as_sf(migr) %>%
+  st_intersection(st_as_sf(a)) %>%
+  as_Spatial() %>%
+  terra::area() %>%
+  sum()#/1000000
+ibaAreaAoi <- st_as_sf(iba) %>%
+  st_intersection(st_as_sf(a)) %>%
+  as_Spatial() %>%
+  terra::area() %>%
+  sum()#/1000000
 
 (migrPercAoi <- migrAreaAoi/migrArea)
 (ibaPercAoi <- ibaAreaAoi/ibaArea)
@@ -26,3 +36,6 @@ ibaAreaAoi <- st_as_sf(iba) %>% st_intersection(st_as_sf(a)) %>%
 (wyoArea <- wyo %>%  terra::area() %>% sum())#/1000000
 
 (aoiPercWyo <- aoiArea/wyoArea)
+
+(aoiArea/1000000*acre_in_km)
+
